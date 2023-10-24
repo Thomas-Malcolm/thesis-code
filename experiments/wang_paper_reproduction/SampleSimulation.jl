ac4b32b069a788dd5a6493af3c447f70160b6a21
Thomas-Malcolm <thomasmalcolm01@gmail.com>
2023-10-25 02:04:25 +1000

    Chapter 3 plots

--- experiments/wang_paper_reproduction/SampleSimulation.jl
+++ experiments/wang_paper_reproduction/SampleSimulation.jl
@@ -10,11 +10,13 @@ z_range = cfg.z_range
 # Figure 1
 # params = Parameters(-0.04531, -1.0808, 2.1683)
 # Figure 2
-params = Parameters(0.01, 3.1, 5.566)
+#params = Parameters(0.01, 3.1, 5.566)
+# Figure 3
+params = Parameters(-0.06, -0.0006, 5.50)
 
 # Magnetic Field Lines
 mag_field = magnetic_field_for_params(cfg, params)
-contour(x_range, z_range, mag_field, title = "Wang Figure 2", xlabel = "x", ylabel = "z,j,p")
+contour(x_range, z_range, mag_field, title = "Wang Figure 3", xlabel = "x", ylabel = "z,j,p")
 
 # Current Profile
 curr_prof = current_profile_for_params_normalised(cfg, params)
@@ -25,4 +27,4 @@ pres_prof = pressure_profile_for_params_normalised(cfg, params)
 plot!(x_range, pres_prof, label = "p")
 
 
-savefig("imgs/figure-2.pdf")
+savefig("imgs/figure-3.pdf")
